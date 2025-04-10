fc8bf10d3d660d4ae7d36f7822d304b99f421f06
DSM2 <fatec-dsm2@academico.intra>
2025-04-10 19:05:52 -0300

    @larissatoyohashi

--- controllers/ProdutosController.js
+++ controllers/ProdutosController.js
@@ -5,11 +5,11 @@ const router = express.Router();
 router.get("/produtos", (req, res) => {
 // Array com lista de Produtos
 const produtos = [
-    {nome: "Orgulho e Preconceito", preco: 39.90, categoria: "Romance"},
-    {nome: "Razão e Sensibilidade", preco: 29.90, categoria: "Romance"},
-    {nome: "Emma", preco: 39.90, categoria: "Romance"},
-    {nome: "It a Coisa", preco: 60.00, categoria: "Terror"},
-    {nome: "O Homem de Giz", preco: 30.00, categoria: "Suspense" },
+    {nome: "Orgulho e Preconceito", preco: 39.90, categoria: "Romance", imagem:"images/orgulhoepreconceito.jpg"},
+    {nome: "Razão e Sensibilidade", preco: 29.90, categoria: "Romance", imagem:"images/razaoesensibilidade.jpg"},
+    {nome: "Emma", preco: 39.90, categoria: "Romance", imagem:"images/emma.jpg"},
+    {nome: "It a Coisa", preco: 60.00, categoria: "Terror", imagem:"images/itacoisa.jpg"},
+    {nome: "O Homem de Giz", preco: 30.00, categoria: "Suspense", imagem:"images/homemdegiz.jpg"},
 ];
 res.render("produtos", {
     // Enviando a variável produto para a página
